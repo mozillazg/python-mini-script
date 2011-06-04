918dea5bd644ccf068e397961e5b93dfe6c71e19
mozillazg <opensource.mozillazg@gmail.com>
2011-06-04 13:09:02 +0800

    os.walk 代替 os.listdir 使用 os.path.join

--- movefile/movefile.py
+++ movefile/movefile.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """移动文件夹到相应日期目录下。
-例如：test.txt 创建于：2011/06/03
+例如：test.txt 最后修改时间：2011/06/03
 移动到目录 2011/06 下
 """
 
@@ -13,11 +13,10 @@ import time
 def file_paths(dir_path):
     """生成器-查找目录下的文件
     """
-    list_dirs = os.listdir(dir_path) # 获取给定目录下的文件及文件夹
-    for  f in list_dirs:
-        f = os.path.realpath(dir_path + '/' + f)
-        if os.path.isfile(f):
-            yield f
+    for root, dirs, files in os.walk(dir_path):
+        dirs[:] = [] # 忽略子目录
+        for f in files:
+            yield os.path.join(root, f)
 
 def move_file(file_path):
     """移动文件到相应的日期目录下
@@ -27,14 +26,11 @@ def move_file(file_path):
             move_file(f)
     elif  os.path.isfile(file_path):
         date_time  = time.strftime('%Y/%m',time.localtime(
-                 os.path.getctime(file_path)  # 文件创建时间
+                 os.path.getmtime(file_path)  # 文件最后修改时间
                  ))
+        dirname = os.path.dirname(file_path)
         basename = os.path.basename(file_path)
-        dirpath = os.path.realpath(os.path.dirname(file_path) + '/' + date_time)
-        new_filepath = os.path.realpath(dirpath+'/'+basename)
-        #if not os.path.exists(dirpath):
-        #os.makedirs(dirpath)
-        #os.system(' move "%s" "%s" ' % (file_path, new_filepath))
+        new_filepath = os.path.join(dirname, date_time, basename)
         os.renames(file_path, new_filepath)
 
 def main():
