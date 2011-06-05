cf4d9836eb0336d58a048a944a22aa17f7aded5c
mozillazg <opensource.mozillazg@gmail.com>
2011-06-05 10:14:21 +0800

    细微调整

--- movefile/movefile.py
+++ movefile/movefile.py
@@ -9,26 +9,26 @@
 import os
 import time
 
-def move_file(file_path):
+def move_file(path):
     """移动文件到相应的日期目录下
     """
-    if os.path.isdir(file_path):
-        for root, dirs, files in os.walk(file_path):
+    if os.path.isdir(path):
+        for root, dirs, files in os.walk(path):
             dirs[:] = [] # 忽略子目录
             for f in files:
                 move_file(os.path.join(root, f))
-    elif  os.path.isfile(file_path):
+    elif  os.path.isfile(path):
         date_time  = time.strftime('%Y/%m',time.localtime(
-                 os.path.getmtime(file_path)  # 文件最后修改时间
+                 os.path.getmtime(path)  # 文件最后修改时间
                  ))
-        dirname = os.path.dirname(file_path)
-        basename = os.path.basename(file_path)
-        new_filepath = os.path.join(dirname, date_time, basename)
-        os.renames(file_path, new_filepath)
+        dirname = os.path.dirname(path)
+        basename = os.path.basename(path)
+        new_path = os.path.join(dirname, date_time, basename)
+        os.renames(path, new_path)
 
 def main():
-    filepath = raw_input("please input the file or dir path:")
-    move_file(filepath)
+    path = raw_input("please input the file or dir path:")
+    move_file(path)
 
 if __name__ == '__main__':
     main()
