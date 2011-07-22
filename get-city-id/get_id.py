b9c7bc83372cbff556144468b8e37b01159697ec
mozillazg <opensource.mozillazg@gmail.com>
2011-07-22 12:29:24 +0800

    修正输入汉字时的编码问题导致查询结果错误

--- get-city-id/get_id.py
+++ get-city-id/get_id.py
@@ -13,7 +13,7 @@ def get_id(keyword, debug=False):
             'callback': 'jsonp1308665312716',
             '_': '1308665332660',
             'language': 'zh',
-            'keyword': str(keyword)
+            'keyword': str(keyword).decode('gbk').encode('utf8')
             })
     url = 'http://toy.weather.com.cn/SearchBox/searchBox?%s' % query
     data = urllib2.urlopen(url).read()
@@ -38,10 +38,6 @@ def get_id(keyword, debug=False):
 def main():
     keyword = raw_input('input(输入城市名、全拼、简拼、电话区号、邮编查询):').strip()
     name, id = get_id(keyword, debug=True)
-    if name:
-        print '\n'.join([
-                name[n] + ':' + id[n] for n in range(len(name))
-            ]).decode('utf8').encode('gbk')
 
 if __name__ == '__main__':
     main()
