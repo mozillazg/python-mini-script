3191c0f166d83d7f32774dd30354e8be959819ed
mozillazg <opensource.mozillazg@gmail.com>
2011-10-27 10:29:46 +0800

    格式化输出

--- weather/weather.py
+++ weather/weather.py
@@ -25,15 +25,19 @@ def weather(cityname):
 
     rex = r'<string>([^<.]*?)</string>'
 
+    city_info = re.findall(r'<string>(.*?)</string>', text)[0]
     weathers = re.findall(rex, text)[7:]
 
-    return weathers
+    return (city_info, weathers)
 
 if __name__ == '__main__':
     cityname = raw_input('input:').strip().decode('gbk').encode('utf8')
-    weathers = weather(cityname)
+    city_info, weathers = weather(cityname)
     print cityname.decode('utf8')
-    if not weathers:
-        print u'查询结果为空'
-    else:
-        print '\n'.join(weathers)
+    if len(weathers) > 1:
+        for a in xrange(0, len(weathers)):
+            weathers[a] = ' ' + weathers[a]
+            if (a+1)%3 == 0:
+                weathers[a] = weathers[a] + '\n'
+    print ''.join(city_info)
+    print ''.join(weathers)
